8805cc9db38c8a744354bd3b8acd98356176edb6
MalesSebastian <malessebastian@gmail.com>
2016-08-04 15:16:53 +0300

    Driver form view

--- config/drivers/views.py
+++ config/drivers/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import DriverForm
+
+
+def submit_driver(request):
+    form = DriverForm(data=request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.instance = request.user
+            form.save()
+    return render(request, "template_here", {
+        'form': form})
