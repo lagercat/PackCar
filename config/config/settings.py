d97a3ab1d5a193e9282a258f3c0776d729d4a5fa
MalesSebastian <malessebastian@gmail.com>
2016-08-04 12:44:53 +0300

    Driver app and phonenumber added to settings

--- config/config/settings.py
+++ config/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
+    'phonenumber_field',
+    'drivers'
 ]
 
 MIDDLEWARE_CLASSES = [
