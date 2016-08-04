fba241c13feb34283ec4482562f09943f957687f
MalesSebastian <malessebastian@gmail.com>
2016-08-04 12:49:15 +0300

    Driver model

--- config/drivers/models.py
+++ config/drivers/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Driver(models.Model):
+    departure = models.CharField(null=True, max_length=300)
+    arrival = models.CharField(null=True, max_length=300)
+    departure_date = models.DateField(null=True)
+    departure_time = models.TimeField(null=True)
+    arrival_date = models.DateField(null=True)
+    arrival_time = models.TimeField(null=True)
+    phonenumber = PhoneNumberField(null=True)
+    author = models.ForeignKey(User, related_name='drivers')
