547801fb5d018f8fe30450ad39173affa38e3cf2
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-08-04 12:43:08 +0300

    login

---
+++ config/authentication/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=30, label='Username',
+                               widget=forms.TextInput(attrs={
+                                   'required': 'required',
+                                   'placeholder': "Username"}))
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput(
+        attrs={'required': 'required',
+               'placeholder': "Password"}),
+                               label='Password')
--- config/authentication/views.py
+++ config/authentication/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+from .forms import LoginForm
+
 
 # Create your views here.
+
+@login_required
+def login_page(request):
+    errors = []
+    form = LoginForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
+            else:
+                errors.append('Incorrect username or password')
+
+        else:
+            errors.append('Invalid form')
+    return render(request, "authentication/logIn.html", {
+        'form': form,
+        'errors': errors})
