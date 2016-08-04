84014099027a56a2dc87d5ed9f27aeff77656d94
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-08-04 12:56:16 +0300

    test login

--- config/authentication/tests.py
+++ config/authentication/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .forms import LoginForm
+
+
+class AuthFormsTestCase(TestCase):
+
+    def setup(self):
+        self.user = User.objects.create_user(username="tester",
+                                             password="top_secret")
+
+    def test_valid_login_form(self):
+        form_data = {'username': 'tester', 'password': 'top_seret'}
+        form = LoginForm(data=form_data)
+        self.assertTrue(form.is_valid())
