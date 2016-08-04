962ee57062b6ef7a0518d780a7967819bf2ede82
MalesSebastian <malessebastian@gmail.com>
2016-08-04 13:16:15 +0300

    Arrival and departure validation

--- config/drivers/forms.py
+++ config/drivers/forms.py
@@ -10,3 +10,21 @@ class DriverForm(forms.ModelForm):
         fields = ['departure', 'arrival', 'departure_date',
                   'departure_time', 'arrival_date', 'arrival_time',
                   'phonenumber']
+
+    def clean_departure(self):
+        data = self.cleaned_data
+        if not data['departure'][0].isupper():
+            raise forms.validators("Departure location"
+                                   "should start wtih capital letter")
+        if not data['departure'][1:].islower():
+            raise forms.ValidationError("The only capital letter should"
+                                        "be the first one")
+
+    def clean_arrival(self):
+        data = self.cleaned_data
+        if not data['arrival'][0].isupper():
+            raise forms.ValidationError("Arrival location"
+                                        "shold start with a capital letter")
+        if not data['departure'][1:].islower():
+            raise forms.ValidationError("The only capital letter should"
+                                        "be the first one")
