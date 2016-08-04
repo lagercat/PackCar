2a55bd76018c0b0748e50a773d0916cff8bad9be
MalesSebastian <malessebastian@gmail.com>
2016-08-04 13:23:13 +0300

    Departure and arrival date validation

--- config/drivers/forms.py
+++ config/drivers/forms.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django import forms
 
 from .models import Driver
@@ -28,3 +30,16 @@ class DriverForm(forms.ModelForm):
         if not data['departure'][1:].islower():
             raise forms.ValidationError("The only capital letter should"
                                         "be the first one")
+
+    def clean_departure_date(self):
+        data = self.cleaned_data
+        if data['departure_date'] < date.now():
+            raise forms.ValidationError("Enter a valid date")
+
+    def clean_arrival_date(self):
+        data = self.cleaned_data
+        if data['arrival_date'] < data.now():
+            raise forms.ValidationError("Enter a valid date")
+        elif data['arrival_date'] < data['departure_date']:
+            raise forms.ValidationError("Arrival can't be"
+                                        "before departure date")
