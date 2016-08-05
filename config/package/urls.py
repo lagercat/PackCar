f50e362a8abddd48d33d41382247f113c195be64
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-08-05 03:38:51 +0300

    page package

--- config/package/urls.py
+++ config/package/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^packages/', views.submit_package, name="submit_package"),
-    url(r'^allpackages/', views.list_packages, name="list_packages")
+    url(r'^allpackages/', views.list_packages, name="list_packages"),
+    url(r'^package/(?P<slug>[^\.]+)/$', views.package, name='package')
 ]
